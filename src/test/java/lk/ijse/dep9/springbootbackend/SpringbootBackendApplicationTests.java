3677573ee12aa1dea276cdd2eafb1c09cd6f8471
SureshSamarawarna <suresh.asan8@gmail.com>
2023-01-31 13:57:38 +0530

    set up the project

---
+++ src/test/java/lk/ijse/dep9/springbootbackend/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep9.springbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
